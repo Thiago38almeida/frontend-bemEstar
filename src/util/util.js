e8c0a921b2c67a13741be19f1fd5c93ce0795bb5
Thiago38almeida <68130910+Thiago38almeida@users.noreply.github.com>
2023-09-20 14:42:00 -0300

    Update util.js

--- src/util/util.js
+++ src/util/util.js
@@ -1,6 +1,6 @@
 //export function refrestPage() { window.location.reload() }
 
-const api = process.env.REACT_APP_API_URL || 'http://172.18.0.5:9091/bemEstar';
+const api = process.env.REACT_APP_API_URL || 'calendario-bem-estar-b4iu.vercel.app';
 var XLSX = require('xlsx')
 const moment = require('moment')
 
